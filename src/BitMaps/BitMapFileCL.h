221ae421b44fb3fb2478bdd12a3e8f16e849a3b7
hhhh hgfh <dania.lukin@gmail.com>
2024-10-10 19:12:49 +0300

    feat(4): rework function of write and read header of archive. Add function of write and read info of file in archive

--- src/BitMaps/BitMapFileCL.h
+++ src/BitMaps/BitMapFileCL.h
@@ -1,32 +1,29 @@
 #ifndef CLARCHIVER_BITMAPFILECL_H
 #define CLARCHIVER_BITMAPFILECL_H
 #include <stdio.h>
+#include <stdint.h>
 
 /*Header of '*.cl' archive*/
 struct BitMapFileCL{
-    unsigned int countOfFiles;
-    size_t* offsetOfFiles;
+    uint16_t countOfFiles;
+    uint32_t* offsetOfFiles;
 };
 
-/*Write header of archive to file*/
-void WriteBitMapFileCL(struct BitMapFileCL* bitMapFileCl, FILE* file){
-    fseek(file,0,SEEK_SET);
-    fwrite(&bitMapFileCl->countOfFiles,sizeof(unsigned int),1,file);
 
-    for(unsigned int i=0;i<bitMapFileCl->countOfFiles;i++){
-        fwrite(&bitMapFileCl->offsetOfFiles[i],sizeof(size_t),1,file);
-    }
+/*Write header of archive to file*/
+void WriteBitMapFileCL(const struct BitMapFileCL* bitMapFileCl, FILE* file){
+    fseek(file,0,SEEK_SET);
+    fwrite(&bitMapFileCl->countOfFiles,sizeof(uint16_t),1,file);
+
+    fwrite(bitMapFileCl->offsetOfFiles,sizeof(uint32_t)*bitMapFileCl->countOfFiles,1,file);
 }
 
 /*Read header of archive from file*/
 void ReadBitMapFileCL(struct BitMapFileCL* bitMapFileCl, FILE* file){
     fseek(file,0,SEEK_SET);
-    fread(&bitMapFileCl->countOfFiles, sizeof(unsigned int),1,file);
+    fread(&bitMapFileCl->countOfFiles, sizeof(uint16_t),1,file);
 
-    bitMapFileCl->offsetOfFiles = (size_t*)malloc(sizeof(size_t)*bitMapFileCl->countOfFiles);
-
-    for(unsigned int i=0;i<bitMapFileCl->countOfFiles;i++){
-        fread(&bitMapFileCl->offsetOfFiles[i],sizeof(size_t),1,file);
-    }
+    bitMapFileCl->offsetOfFiles = (uint32_t*)malloc(sizeof(uint32_t)*bitMapFileCl->countOfFiles);
+    fread(bitMapFileCl->offsetOfFiles,sizeof(uint32_t) * bitMapFileCl->countOfFiles,1,file);
 }
 #endif //CLARCHIVER_BITMAPFILECL_H
--- src/BitMaps/BitMapFilesInfo.h
+++ src/BitMaps/BitMapFilesInfo.h
@@ -1,15 +1,51 @@
 #ifndef CLARCHIVER_BITMAPFILESINFO_H
 #define CLARCHIVER_BITMAPFILESINFO_H
 #include <stdio.h>
+#include <stdint.h>
 
 struct BitMapFilesInfo{
-    unsigned char nameOfFile[20];
-    size_t sizeOfFile;
-    unsigned int sizeOfAlphabet;
-    unsigned char* symbols;
-    unsigned char* countOfThatSymbol;
+    uint8_t nameOfFile[20];
+    uint32_t sizeOfFile;
+    uint16_t sizeOfAlphabet;
+    uint8_t * symbols;
+    uint32_t * countOfThatSymbol;
 };
 
 
+
+void WriteBitMapFileInfo(const struct BitMapFilesInfo* bitMapFilesInfo,FILE* file,const __int32* offsetOfFile){
+    fseek(file,*offsetOfFile,SEEK_SET);
+    fwrite(&bitMapFilesInfo,
+           sizeof(uint8_t)*20 + sizeof(uint32_t) + sizeof(uint16_t),
+           1,
+           file);
+    fwrite(&bitMapFilesInfo->symbols,
+           sizeof(uint8_t) * bitMapFilesInfo->sizeOfAlphabet,
+           1,
+           file);
+    fwrite(&bitMapFilesInfo->countOfThatSymbol,
+           sizeof(uint32_t) * bitMapFilesInfo->sizeOfAlphabet,
+           1,
+           file);
+}
+
+void ReadBitMapFileInfo(struct BitMapFilesInfo* bitMapFilesInfo,FILE* file,const __int32* offsetOfFile){
+    fseek(file,*offsetOfFile,SEEK_SET);
+    fread(&bitMapFilesInfo,
+           sizeof(uint8_t)*20 + sizeof(uint32_t) + sizeof(uint16_t),
+           1,
+           file);
+
+    bitMapFilesInfo->symbols = (uint8_t*) malloc(sizeof(uint8_t)*bitMapFilesInfo->sizeOfAlphabet);
+    bitMapFilesInfo->countOfThatSymbol = (uint32_t*) malloc(sizeof(uint32_t)*bitMapFilesInfo->sizeOfAlphabet);
+    fread(&bitMapFilesInfo->symbols,
+           sizeof(uint8_t) * bitMapFilesInfo->sizeOfAlphabet,
+           1,
+           file);
+    fread(&bitMapFilesInfo->countOfThatSymbol,
+           sizeof(uint32_t) * bitMapFilesInfo->sizeOfAlphabet,
+           1,
+           file);
+}
 
 #endif //CLARCHIVER_BITMAPFILESINFO_H
