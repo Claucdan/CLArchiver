1db37b81cf1775d1026fc92eef083ca5171bd488
hhhh hgfh <dania.lukin@gmail.com>
2024-10-10 18:11:50 +0300

    feat(2): add function for read and write header information from/to file

--- src/BitMaps/BitMapFileCL.h
+++ src/BitMaps/BitMapFileCL.h
@@ -1,9 +1,32 @@
 #ifndef CLARCHIVER_BITMAPFILECL_H
 #define CLARCHIVER_BITMAPFILECL_H
-#include <typeinfo.h>
+#include <stdio.h>
 
+/*Header of '*.cl' archive*/
 struct BitMapFileCL{
     unsigned int countOfFiles;
     size_t* offsetOfFiles;
 };
+
+/*Write header of archive to file*/
+void WriteBitMapFileCL(struct BitMapFileCL* bitMapFileCl, FILE* file){
+    fseek(file,0,SEEK_SET);
+    fwrite(&bitMapFileCl->countOfFiles,sizeof(unsigned int),1,file);
+
+    for(unsigned int i=0;i<bitMapFileCl->countOfFiles;i++){
+        fwrite(&bitMapFileCl->offsetOfFiles[i],sizeof(size_t),1,file);
+    }
+}
+
+/*Read header of archive from file*/
+void ReadBitMapFileCL(struct BitMapFileCL* bitMapFileCl, FILE* file){
+    fseek(file,0,SEEK_SET);
+    fread(&bitMapFileCl->countOfFiles, sizeof(unsigned int),1,file);
+
+    bitMapFileCl->offsetOfFiles = (size_t*)malloc(sizeof(size_t)*bitMapFileCl->countOfFiles);
+
+    for(unsigned int i=0;i<bitMapFileCl->countOfFiles;i++){
+        fread(&bitMapFileCl->offsetOfFiles[i],sizeof(size_t),1,file);
+    }
+}
 #endif //CLARCHIVER_BITMAPFILECL_H
